36cf3ea499a0171913f824a250227b4de96656d2
Dominik <dominik-nowak@hotmail.com>
2021-01-23 05:59:50 +0100

    create class Rabbit

---
+++ src/Animals/Rabbit.ts
@@ -0,0 +1,8 @@
+import { AnimalNames } from '../Enums/enumAnimalNames';
+import { Animal } from './Animal';
+
+export class Rabbit extends Animal {
+  constructor() {
+    super(AnimalNames.RABBIT, 1);
+  }
+}
