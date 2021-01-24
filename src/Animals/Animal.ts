743d15ab098ffb9c469a1b6310bdc1ccc642767e
Dominik <dominik-nowak@hotmail.com>
2021-01-24 23:52:51 +0100

    create addRole and removeRole in Animal

--- src/Animals/Animal.ts
+++ src/Animals/Animal.ts
@@ -1,13 +1,44 @@
 import { AnimalNames } from '../Enums/AnimalNamesEnum';
+import { AnimalRoles } from '../Enums/AnimalRolesEnum';
+import _ from 'lodash';
 
 export abstract class Animal {
+  protected name: AnimalNames;
+  protected roles: AnimalRoles[];
+  protected value?: number;
+
   constructor(
-    protected name: AnimalNames,
-    protected value?: number,
-  ) {}
+    name: AnimalNames,
+    value?: number,
+    ...roles: AnimalRoles[]
+  ) {
+    this.name = name;
+    this.roles = roles;
+    this.value = value;
+  }
 
   get theName(): string {
     return this.name;
+  }
+
+  get theRoles(): AnimalRoles[] {
+    return this.roles;
+  }
+
+  /**
+   * @param role - one role or more to push to the roles
+   * @returns new lenght of the array
+   */
+  addRole(...role: AnimalRoles[]): number {
+    return this.roles.push(...role);
+  }
+
+  /**
+   * @param role - one role or more to remove from the roles
+   * @returns the array after removing elements
+   */
+  removeRole(...role: AnimalRoles[]): AnimalRoles[] {
+    return _.pull(this.roles, ...role);
   }
 
   get theValue(): number {
