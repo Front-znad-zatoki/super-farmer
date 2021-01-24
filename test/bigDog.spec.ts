dacd571f8c52882c263cfb44570db94296a4c924
Dominik <dominik-nowak@hotmail.com>
2021-01-24 22:20:21 +0100

    add test to bigDog

---
+++ test/bigDog.spec.ts
@@ -0,0 +1,11 @@
+import { BigDog } from '../src/Animals/BigDog';
+
+const bigDog = new BigDog();
+
+describe('should return proper string', function () {
+  it('should pass', function () {
+    expect(bigDog.protectHerd()).toBe(
+      `${bigDog.theName}: WOOF! WOOF! I'm saving the whole herd! WOOF! WOOF!`,
+    );
+  });
+});
