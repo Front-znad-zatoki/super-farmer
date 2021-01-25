d3a966dbd321d8f079c02ae8fb3710d84824954b
VieraBoschkova <vieraboschkova@gmail.com>
2021-01-25 23:24:21 +0100

    feat: implement basic methods

--- src/app/logic/Herd.ts
+++ src/app/logic/Herd.ts
@@ -1,7 +1,28 @@
+import { Animal } from '../../Animals/Animal';
+import { BigDog } from '../../Animals/BigDog';
+import { Cow } from '../../Animals/Cow';
+import { Fox } from '../../Animals/Fox';
+import { Horse } from '../../Animals/Horse';
+import { Pig } from '../../Animals/Pig';
+import { Rabbit } from '../../Animals/Rabbit';
+import { Sheep } from '../../Animals/Sheep';
+import { SmallDog } from '../../Animals/SmallDog';
+import { Wolf } from '../../Animals/Wolf';
+import { AnimalNames } from '../../Enums/AnimalNamesEnum';
 
-class Herd {
+export class Herd {
+  // protected animals: [
   protected animals: [Animal, number][];
+  // [Rabbit, number],
+  // [Sheep, number],
+  // [Pig, number],
+  // [Cow, number],
+  // [Horse, number],
+  // [SmallDog, number],
+  // [BigDog, number],
+  // ];
   //OR protected rabbit: [new Rabbit(), number]
+  //OR new Map()?
   constructor(
     numberOfRabbits = 0,
     numberOfSheep = 0,
@@ -22,66 +43,80 @@ class Herd {
     ];
   }
 
-  // changeAnimalsState(animalName, newNumber):void {
-
-  // }
-
-  addAnimals(animalName: AnimalNames, numberToAdd: number): void {
+  addAnimals(animalName: AnimalNames, numberToAdd: number): Herd {
     //TODO REFACTOR CODE
-    this.animals.map((animalData) => {
-      const { name } = animalData[0];
+    this.animals = this.animals.map((animalData) => {
+      // const { name } = animalData[0]; // name protected
+      const name = animalData[0].theName;
       if (name === animalName) {
         animalData[1] += numberToAdd;
       }
+      return animalData;
     });
+    return this;
   }
 
-  get theAnimals() {
+  get theAnimals(): [Animal, number][] {
     return this.animals;
   }
-  // get animalData(animalName: AnimalNames): [Animal, number] {
-    // TODO get the tuple with data
-    // return [Animal, number]
-  // }
 
-  get animalNumber(animalName: AnimalNames): number {
+  getAnimalNumber(animalName: AnimalNames): number {
     //TODO REFACTOR CODE
     const animalToCheck = this.animals.find((animalData) => {
-      animalData[0].name === animalName;
+      return animalData[0].theName === animalName;
     });
-    // if (!animalToCheck) throw new Error ('no such animal in this herd')
     if (!animalToCheck) return 0;
     return animalToCheck[1];
   }
 
+  private cullAllAnimalsOfOneType(animalName: AnimalNames): void {
+    const currentNumberOfAnimals = this.getAnimalNumber(animalName);
+    this.addAnimals(animalName, -currentNumberOfAnimals);
+  }
+
+  private cullAllAnimalsOfGivenTypes(
+    animalNames: AnimalNames[],
+  ): void {
+    animalNames.forEach((animal) => {
+      this.cullAllAnimalsOfOneType(animal);
+    });
+  }
+
   cullAnimals(attackingAnimal: Fox | Wolf): void {
     //TODO REFACTOR CODE
-    switch (attackingAnimal.name) {
+    switch (attackingAnimal.theName) {
       //TODO CHECK IF PASSED ANIMAL IS AN OBJECT, ADJUST CASES ACCORDINGLY (EX.: CASE )
-      case 'fox': {
-        const smallDogData = this.animals.find((animalData) => {
-          animalData[0].name === 'smallDog';
-        });
-        if (!smallDogData || smallDogData[1] < 1) {
-          Fox.clearHerd();
+      case AnimalNames.FOX: {
+        const hasSmallDog =
+          this.getAnimalNumber(AnimalNames.SMALL_DOG) > 0;
+        if (!hasSmallDog) {
+          attackingAnimal.attackHerd();
           // TODO - get the rabbits in animals and change to zero
-          // return;
+          this.cullAllAnimalsOfOneType(AnimalNames.RABBIT);
+          return;
         }
-        this.addAnimals('smallDog', -1);
-        // TODO - get the small dog object from animals and fire the method protect herd 'WOOF WOOF'
+        this.addAnimals(AnimalNames.SMALL_DOG, -1);
+        // this.animals[5][0].protectHerd();
+
         break;
       }
-      case 'wolf': {
-        const bigDogsData = this.animals.find((animalData) => {
-          animalData[0].name === 'bigDog';
-        });
-        if (!bigDogsData || bigDogsData[1] < 1) {
-          Wolf.clearHerd();
-          // TODO - get all the animals in animals and change to zero except for horse
-          // return;
+      case AnimalNames.WOLF: {
+        const hasBigDog =
+          this.getAnimalNumber(AnimalNames.BIG_DOG) > 0;
+        if (!hasBigDog) {
+          attackingAnimal.attackHerd();
+          // TODO - REFACTOR get all the animals in animals and change to zero except for horse
+          this.cullAllAnimalsOfGivenTypes([
+            AnimalNames.COW,
+            AnimalNames.PIG,
+            AnimalNames.RABBIT,
+            AnimalNames.SHEEP,
+          ]);
+          return;
         }
-        this.addAnimals('bigDog', -1);
-        // TODO - get the big dog object from animals and fire the method protect herd (WOOOOOF WOOOOOF)
+        this.addAnimals(AnimalNames.BIG_DOG, -1);
+        // TODO - REFACTOR get the big dog object from animals and fire the method protect herd (WOOOOOF WOOOOOF)
+        // this.animals[6][0].protectHerd();
       }
     }
   }
