aaa1bb295d082a6cca2d50daedb21346524775b3
paula0403 <paulinalipinskagdy@gmail.com>
2021-01-27 17:25:08 +0100

    Changed App.ts

--- src/app/App.ts
+++ src/app/App.ts
@@ -2,12 +2,14 @@ import { View } from './View';
 
 export class App {
   init(): string {
-    const vieew = new View().renderGameView(
-      'AJAJAJ',
-      `../../resources/images/avatars/dog.png`,
-    );
-    console.log(vieew);
-    return 'hello world';
+    {
+      const vieew = new View().renderGameView(
+        'AJAJAJ',
+        `../../resources/images/avatars/dog.png`,
+      );
+      console.log(vieew);
+      return 'hello world';
+    }
+    //view = new View().renderMenuView();
   }
-  //view = new View().renderMenuView();
 }
