a788785aaa300c4eed6952a6efc9ddc4a37aec94
Enessetere <muszalski.sebastian@gmail.com>
2021-01-27 13:43:11 +0100

    test: deploy test

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,5 +1,14 @@
+import { Render } from "./utils/Render";
+
 export class App {
   init(): string {
+    Render.render(
+      '#sf-app',
+      Render.elementFactory('img', {
+        src: '/static/assets/images/avatars/sheep.png',
+        alt: 'avatar',
+      }),
+    );
     return 'hello world';
   }
 }
