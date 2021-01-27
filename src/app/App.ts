96f825439a5563b1b9b3bfacedcb9f2c32955fd9
VieraBoschkova <vieraboschkova@gmail.com>
2021-01-28 00:36:39 +0100

    feat: basic modal structure

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,7 +1,14 @@
+import { ModalBasic } from './components/ModalBasic';
 import { Render } from './utils/Render';
 
 export class App {
   init(): string {
+    const basicModal: HTMLElement = new ModalBasic().renderBasicModal(
+      'START GAME',
+      'Choose your options',
+    );
+    Render.render('#sf-app', basicModal);
+
     return 'hello world';
   }
 }
--- src/app/components/ModalBasic.ts
+++ src/app/components/ModalBasic.ts
@@ -1,1 +1,88 @@
 import { Render } from '../utils/Render';
+
+export class ModalBasic {
+  renderBasicModal(
+    heading: string,
+    text: string,
+    modalContent?: HTMLElement,
+  ): HTMLElement {
+    const handleModalClose = () => {
+      //TODO: WRITE LOGIC
+      Render.removeElement('.modal');
+    };
+    const handlePlayGame = () => {
+      //TODO: WRITE LOGIC
+      Render.removeElement('.modal');
+    };
+    // CREATE MAIN ELEMENTS
+    const modalHeader = Render.elementFactory(
+      'h2',
+      { className: 'modal__heading' },
+      heading,
+    );
+    const modalText = Render.elementFactory(
+      'p',
+      {
+        className: 'modal__text',
+      },
+      text,
+    );
+    const buttonsRow = this.renderBottomButtons(
+      'GO BACK',
+      handleModalClose,
+      'PLAY',
+      handlePlayGame,
+    );
+    const modalContainer = Render.elementFactory(
+      'div',
+      {
+        className: 'modal__container',
+      },
+      modalHeader,
+      modalText,
+      modalContent ? modalContent : '',
+      buttonsRow,
+    );
+    const modal = Render.elementFactory(
+      'div',
+      { className: 'modal' },
+      modalContainer,
+    );
+    return modal;
+  }
+
+  renderBottomButtons(
+    leftButtonText: string,
+    leftButtonAction: () => void,
+    rightButtonText: string,
+    rightButtonAction: () => void,
+  ): HTMLElement {
+    const leftButton = Render.elementFactory(
+      'button',
+      {
+        className: 'button modal__button',
+        type: 'button',
+      },
+      leftButtonText,
+    );
+    const rightButton = Render.elementFactory(
+      'button',
+      {
+        className: 'button modal__button',
+        type: 'button',
+      },
+      rightButtonText,
+    );
+    leftButton.addEventListener('click', leftButtonAction);
+    rightButton.addEventListener('click', rightButtonAction);
+    const buttonsRow = Render.elementFactory(
+      'div',
+      {
+        className: 'modal__buttons',
+      },
+      leftButton,
+      rightButton,
+    );
+    return buttonsRow;
+  }
+}
