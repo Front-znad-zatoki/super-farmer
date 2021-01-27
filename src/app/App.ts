423d182dbb56fa521cb1da2f5bed4dd3397df354
Enessetere <muszalski.sebastian@gmail.com>
2021-01-27 16:49:11 +0100

    chore: cleanup file

--- src/app/App.ts
+++ src/app/App.ts
@@ -2,13 +2,6 @@ import { Render } from './utils/Render';
 
 export class App {
   init(): string {
-    Render.render(
-      '#sf-app',
-      Render.elementFactory('img', {
-        src: './static/images/avatars/sheep.png',
-        alt: 'avatar',
-      }),
-    );
     return 'hello world';
   }
 }
