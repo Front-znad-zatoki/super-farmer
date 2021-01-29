6ddf62e765e4c8dcf6f8612f96459f072fd410f1
Enessetere <muszalski.sebastian@gmail.com>
2021-01-29 22:28:31 +0100

    feat: add trade demo class with static method to display demo

---
+++ src/app/TradeDemo.ts
@@ -0,0 +1,95 @@
+import { AnimalNames } from '~src/Enums/AnimalNamesEnum';
+import { Player } from '~src/Player';
+import { Trade } from './Trade';
+
+export class TradeDemo {
+  static playDemo(): void {
+    const bank = new Player('bank');
+    bank.theHerd.addAnimals(AnimalNames.RABBIT, 60);
+    bank.theHerd.addAnimals(AnimalNames.SHEEP, 24);
+    bank.theHerd.addAnimals(AnimalNames.PIG, 20);
+    bank.theHerd.addAnimals(AnimalNames.COW, 12);
+    bank.theHerd.addAnimals(AnimalNames.HORSE, 4);
+    bank.theHerd.addAnimals(AnimalNames.SMALL_DOG, 4);
+    bank.theHerd.addAnimals(AnimalNames.BIG_DOG, 2);
+    const trade = new Trade(bank);
+    const player = new Player('player');
+    player.theHerd.addAnimals(AnimalNames.SHEEP, 5);
+    player.theHerd.addAnimals(AnimalNames.PIG, 5);
+    player.theHerd.addAnimals(AnimalNames.COW, 2);
+    const offer1: [AnimalNames, number] = [AnimalNames.SHEEP, 1];
+    const offer2: [AnimalNames, number] = [AnimalNames.SHEEP, 2];
+    const offer3: [AnimalNames, number] = [AnimalNames.COW, 1];
+    const offer4: [AnimalNames, number] = [AnimalNames.COW, 2];
+    const offer5: [AnimalNames, number] = [AnimalNames.PIG, 2];
+    const target1: [AnimalNames, number] = [AnimalNames.RABBIT, 6];
+    const target2: [AnimalNames, number] = [AnimalNames.RABBIT, 7];
+    const target3: [AnimalNames, number] = [AnimalNames.RABBIT, 5];
+    const target4: [AnimalNames, number] = [AnimalNames.HORSE, 1];
+    const target5: [AnimalNames, number] = [AnimalNames.RABBIT, 12];
+
+    console.log(
+      `OFFER: ${offer1}; TARGET: ${target1}; SUCCES: ${trade.processOffer(
+        offer1,
+        player,
+        target1,
+      )}; PLAYER_HERD: ${JSON.stringify(
+        player.theHerd,
+      )}; BANK_HERD: ${JSON.stringify(bank.theHerd)};`,
+    );
+    console.log(
+      `OFFER: ${offer1}; TARGET: ${target2}; SUCCES: ${trade.processOffer(
+        offer1,
+        player,
+        target2,
+      )}; PLAYER_HERD: ${JSON.stringify(
+        player.theHerd,
+      )}; BANK_HERD: ${JSON.stringify(bank.theHerd)};`,
+    );
+    console.log(
+      `OFFER: ${offer1}; TARGET: ${target3}; SUCCES: ${trade.processOffer(
+        offer1,
+        player,
+        target3,
+      )}; PLAYER_HERD: ${JSON.stringify(
+        player.theHerd,
+      )}; BANK_HERD: ${JSON.stringify(bank.theHerd)};`,
+    );
+    console.log(
+      `OFFER: ${offer2}; TARGET: ${target1}; SUCCES: ${trade.processOffer(
+        offer2,
+        player,
+        target1,
+      )}; PLAYER_HERD: ${JSON.stringify(
+        player.theHerd,
+      )}; BANK_HERD: ${JSON.stringify(bank.theHerd)};`,
+    );
+    console.log(
+      `OFFER: ${offer3}; TARGET: ${target4}; SUCCES: ${trade.processOffer(
+        offer3,
+        player,
+        target4,
+      )}; PLAYER_HERD: ${JSON.stringify(
+        player.theHerd,
+      )}; BANK_HERD: ${JSON.stringify(bank.theHerd)};`,
+    );
+    console.log(
+      `OFFER: ${offer4}; TARGET: ${target4}; SUCCES: ${trade.processOffer(
+        offer4,
+        player,
+        target4,
+      )}; PLAYER_HERD: ${JSON.stringify(
+        player.theHerd,
+      )}; BANK_HERD: ${JSON.stringify(bank.theHerd)};`,
+    );
+    console.log(
+      `OFFER: ${offer5}; TARGET: ${target5}; SUCCES: ${trade.processOffer(
+        offer5,
+        player,
+        target5,
+      )}; PLAYER_HERD: ${JSON.stringify(
+        player.theHerd,
+      )}; BANK_HERD: ${JSON.stringify(bank.theHerd)};`,
+    );
+  }
+}
