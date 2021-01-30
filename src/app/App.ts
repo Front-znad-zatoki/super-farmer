a36ee6135cd71097fbda47956337319205b77265
paula0403 <paulinalipinskagdy@gmail.com>
2021-01-30 16:36:31 +0100

    Timer and buttons removed from PlayersBoard; styles added.

--- src/app/App.ts
+++ src/app/App.ts
@@ -12,6 +12,7 @@ export class App {
     const view = new PlayersBoard().renderPlayersBoard(
       '../../static/images/avatars/sheep.png',
       'Carlos Santana',
+      'red',
     );
 
     return 'hello world';
--- src/app/components/PlayersBoard.ts
+++ src/app/components/PlayersBoard.ts
@@ -6,123 +6,88 @@ export class PlayersBoard {
   renderPlayersBoard(
     playersChosenAvatarPath: string,
     playersChosenName: string,
+    playersColor: string,
   ): void {
     const playersBoardContainer: HTMLElement = Render.elementFactory(
       'div',
       {
-        className: 'board__container',
+        className: 'board-container',
       },
     );
-    const rollDiceButton: HTMLElement = Render.elementFactory(
-      'button',
-      {
-        className: 'button__dice button',
-      },
-      'Roll a dice',
+    const playersDataPanel: HTMLElement = Render.elementFactory(
+      'div',
+      { className: 'board-container__data' },
     );
-    const exchangeButton: HTMLElement = Render.elementFactory(
-      'button',
+    const playerName = Render.elementFactory(
+      'h3',
       {
-        className: 'button__exchange button',
+        className: 'board-container__data__name',
+        style: `color:${playersColor}`,
       },
-      'Exchange',
+      playersChosenName,
     );
     const playerAvatar = Render.elementFactory('img', {
       className: 'player__avatar',
       src: playersChosenAvatarPath,
     });
     const gameController = new GameController(playersChosenName);
-    const remainingTime = Render.elementFactory(
-      'div',
-      {
-        className: 'remainig-time__counter',
-      },
-
-      `Time Left : ${gameController.theTimer.theTurnTimeLeft} s`,
-    );
-    const herdView = Render.elementFactory('div');
-    const rollResultView = Render.elementFactory('div');
-    rollResultView.addEventListener('click', () => {
-      gameController.breed();
+    const herdView = Render.elementFactory('div', {
+      className: 'herd__container',
     });
 
-    setInterval(() => {
-      remainingTime.innerText = `Time Left : ${gameController.theTimer.theTurnTimeLeft} s`;
-      if (gameController.theTimer.theTurnTimeLeft === 0) {
-        alert('Your time is up!');
-        gameController.nextPlayer();
-        gameController.startTurn();
-      }
-    }, 1000);
-    rollDiceButton.addEventListener('click', () => {
-      Render.removeAllChildren(rollResultView);
-      const rollResult = gameController.breed();
-      const playerHerd = gameController.theCurrentPlayer.theHerd.theAnimals.map(
-        ([animal, count]) => [animal.theName, count],
-      );
-      const herdImagesAndCounts = playerHerd.map(
-        ([nameElement, countElement]) => {
-          if (
-            nameElement === 'big dog' ||
-            nameElement === 'small dog'
-          ) {
-            const animalImg = Render.elementFactory('img', {
+    const playerHerd = gameController.theCurrentPlayer.theHerd.theAnimals.map(
+      ([animal, count]) => [animal.theName, count],
+    );
+    const herdImagesAndCounts = playerHerd.map(
+      ([nameElement, countElement]) => {
+        if (
+          nameElement === 'big dog' ||
+          nameElement === 'small dog'
+        ) {
+          const animalImg = Render.elementFactory(
+            // 'div',
+            // { className: 'animal-img__item' },
+            'img',
+            {
               src: `../../../static/images/avatars/dog.png`,
-            });
-            console.log(animalImg, countElement);
-            const animalCount = Render.elementFactory(
-              'div',
-              { className: 'count__div' },
-              countElement as string,
-            );
-
-            return [animalImg, animalCount];
-          }
-          const animalImg = Render.elementFactory('img', {
-            src: `../../../static/images/avatars/${nameElement}.png`,
-          });
+            },
+          );
+          console.log(animalImg, countElement);
           const animalCount = Render.elementFactory(
             'div',
-            { className: 'count__div' },
-            countElement as string,
+            { className: 'count-element__item' },
+            `x${countElement}`,
           );
-          console.log(animalCount);
+
           return [animalImg, animalCount];
-        },
-      );
-
-      console.log(herdImagesAndCounts);
-
-      Render.childrenInjector(
-        herdView,
-        ...flatten(herdImagesAndCounts),
-      );
-
-      const rollResults = rollResult as string[];
-      const diceImages = rollResults.map((result) => {
-        if (result === 'big dog' || result === 'small dog') {
-          return Render.elementFactory('img', {
-            src: `../../../static/Image/avatars/dog.png`,
-          });
         }
-        return Render.elementFactory('img', {
-          src: `../../../static/images/avatars/${result}.png`,
+        const animalImg = Render.elementFactory('img', {
+          src: `../../../static/images/avatars/${nameElement}.png`,
         });
-      });
-      Render.childrenInjector(rollResultView, ...diceImages);
-      gameController.nextPlayer();
-      setTimeout(() => gameController.startTurn(), 1000);
-    });
-    gameController.startTurn();
+        const animalCount = Render.elementFactory(
+          'div',
+          { className: 'count__div' },
+          `x${countElement}`,
+        );
+        console.log(animalCount);
+        return [animalImg, animalCount];
+      },
+    );
 
     Render.childrenInjector(
-      playersBoardContainer,
       herdView,
-      remainingTime,
-      rollDiceButton,
-      rollResultView,
-      exchangeButton,
+      ...flatten(herdImagesAndCounts),
+    );
+
+    Render.childrenInjector(
+      playersDataPanel,
+      playerName,
       playerAvatar,
+    );
+    Render.childrenInjector(
+      playersBoardContainer,
+      playersDataPanel,
+      herdView,
     );
     Render.render('#sf-app', playersBoardContainer);
   }
