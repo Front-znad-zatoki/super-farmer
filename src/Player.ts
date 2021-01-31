f76fabc363d11ec1e9b2e73dc6ceb50050419168
VieraBoschkova <vieraboschkova@gmail.com>
2021-01-31 17:04:18 +0100

    fix: change Players properties

--- src/Player.ts
+++ src/Player.ts
@@ -1,15 +1,26 @@
 import { Herd } from './app/logic/Herd';
+import { AnimalNames } from './Enums/AnimalNamesEnum';
+// import { AnimalRoles } from './Enums/AnimalRolesEnum';
 
 export class Player {
   protected name: string;
   protected avatar: string;
   protected herd: Herd;
+  protected color: string;
 
   // TODO: set path to default avatar when it's available
-  constructor(name: string, avatar = 'path to default avatar') {
+  // TODO: DECIDE HOW THE COLORS SHOULD BE CHOSEN, DEFAULT ALSO
+
+  constructor(
+    name: string,
+    avatar = 'path to default avatar',
+    color = 'bada55',
+    playerHerdConfig: [AnimalNames, number][],
+  ) {
     this.name = name;
-    this.herd = new Herd();
+    this.herd = new Herd(playerHerdConfig);
     this.avatar = avatar;
+    this.color = color;
   }
 
   get theName(): string {
@@ -30,5 +41,13 @@ export class Player {
 
   set theHerd(herd: Herd) {
     this.herd = herd;
+  }
+
+  get theColor(): string {
+    return this.color;
+  }
+
+  set theColor(color: string) {
+    this.color = color;
   }
 }
