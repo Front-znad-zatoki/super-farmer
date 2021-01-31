ebe47753e4a785e86b3db89b611cc39f3aa51d92
VieraBoschkova <vieraboschkova@gmail.com>
2021-01-31 17:06:31 +0100

    fix: adjust bank to the players parameters

--- src/app/logic/Bank.ts
+++ src/app/logic/Bank.ts
@@ -1,9 +1,11 @@
-import { Player } from '~src/Player';
-import { Herd } from './Herd';
+import { AnimalNames } from '../../Enums/AnimalNamesEnum';
+import { Player } from '../../Player';
+// import { Herd } from './Herd';
 
 export class Bank extends Player {
-  constructor() {
-    super('Bank');
-    this.herd = new Herd(60, 24, 20, 12, 4, 4, 2);
+  constructor(banksHerdConfig: [AnimalNames, number][]) {
+    super('Bank', '', 'green', banksHerdConfig);
+    // TODO: DELETE HERD AFTER REFACTOR CONFIRMED
+    // this.herd = new Herd(60, 24, 20, 12, 4, 4, 2);
   }
 }
