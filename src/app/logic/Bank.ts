65557686bedc4f28809ad7fe05d0c3d3d04e02e9
VieraBoschkova <vieraboschkova@gmail.com>
2021-02-01 21:32:47 +0100

    refactor: update herd's methods create banks default config

--- src/app/logic/Bank.ts
+++ src/app/logic/Bank.ts
@@ -1,8 +1,11 @@
 import { HerdConfigInterface } from '~src/Interfaces/HerdConfigInterface';
 import { Player } from '../../Player';
+import { defaultBankConfig } from './defaultBankConfig';
 
 export class Bank extends Player {
-  constructor(banksHerdConfig: HerdConfigInterface[]) {
+  constructor(
+    banksHerdConfig: HerdConfigInterface[] = defaultBankConfig,
+  ) {
     super('bank', '', '', banksHerdConfig);
     // TO BE DIFNED BY GAME'S CONFIG:
     // this.herd = new Herd(60, 24, 20, 12, 4, 4, 2);
