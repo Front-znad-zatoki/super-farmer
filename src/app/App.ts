1c2ff4b81e9c4def634aa4ac6a558ce8345b4656
paula0403 <paulinalipinskagdy@gmail.com>
2021-02-02 13:49:36 +0100

    Develop merged

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,5 +1,10 @@
+import { PlayersBoard } from './components/PlayersBoard';
 export class App {
   init(): string {
+    const view = new PlayersBoard().renderPlayersBoard(
+      'Carlos',
+      `../../../static/images/avatars/dog.png`,
+    );
     return 'hello world';
   }
 }
