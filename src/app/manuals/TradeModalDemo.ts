8983b3bf8c2c10dfbdf68d5b055432f96d53b022
Dominik <dominik-nowak@hotmail.com>
2021-02-04 06:52:09 +0100

    add change players after timeout in TradeModalDemo

--- src/app/manuals/TradeModalDemo.ts
+++ src/app/manuals/TradeModalDemo.ts
@@ -5,11 +5,12 @@ import { Bank } from '../logic/Bank';
 import { Trade } from '../Trade';
 import { Render } from '../utils/Render';
 
+//type TradeModalDemo.playDemo() in init in App.ts
 export class TradeModalDemo {
   static playDemo(): void {
     const trade = new Trade(new Bank());
     const player = new Player(
-      'player',
+      'Donald',
       './static/images/avatars/dog.png',
     );
     player.theHerd.addAnimalsToHerd(AnimalNames.RABBIT, 20);
@@ -19,6 +20,29 @@ export class TradeModalDemo {
     player.theHerd.addAnimalsToHerd(AnimalNames.HORSE, 1);
     const modal = new TradeModal(trade, player);
     Render.render('#sf-app', modal.createModal());
-    modal.setPlayer(player);
+
+    const player2 = new Player(
+      'Gerwazy',
+      './static/images/avatars/dog.png',
+    );
+    player2.theHerd.addAnimalsToHerd(AnimalNames.RABBIT, 6);
+    player2.theHerd.addAnimalsToHerd(AnimalNames.SHEEP, 1);
+    player2.theHerd.addAnimalsToHerd(AnimalNames.PIG, 2);
+    player2.theHerd.addAnimalsToHerd(AnimalNames.COW, 1);
+    player2.theHerd.addAnimalsToHerd(AnimalNames.HORSE, 1);
+
+    setTimeout(() => modal.setNextPlayer(player2), 15000);
+
+    const player3 = new Player(
+      'Eustachy',
+      './static/images/avatars/dog.png',
+    );
+    player3.theHerd.addAnimalsToHerd(AnimalNames.RABBIT, 8);
+    player3.theHerd.addAnimalsToHerd(AnimalNames.SHEEP, 3);
+    player3.theHerd.addAnimalsToHerd(AnimalNames.PIG, 1);
+    player3.theHerd.addAnimalsToHerd(AnimalNames.COW, 2);
+    player3.theHerd.addAnimalsToHerd(AnimalNames.HORSE, 0);
+
+    setTimeout(() => modal.setNextPlayer(player3), 15000);
   }
 }
