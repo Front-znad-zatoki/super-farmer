43497b75f699283d5ed7e45e1df8bbe7ba6e7e5b
Dominik <dominik-nowak@hotmail.com>
2021-02-04 06:14:06 +0100

    update TradeModalDemo

--- src/app/manuals/TradeModalDemo.ts
+++ src/app/manuals/TradeModalDemo.ts
@@ -1,3 +1,4 @@
+import { AnimalNames } from '~src/Enums/AnimalNamesEnum';
 import { Player } from '~src/Player';
 import { TradeModal } from '../components/TradeModal';
 import { Bank } from '../logic/Bank';
@@ -7,11 +8,17 @@ import { Render } from '../utils/Render';
 export class TradeModalDemo {
   static playDemo(): void {
     const trade = new Trade(new Bank());
-    const modal = new TradeModal(trade);
     const player = new Player(
       'player',
       './static/images/avatars/dog.png',
     );
+    player.theHerd.addAnimalsToHerd(AnimalNames.RABBIT, 20);
+    player.theHerd.addAnimalsToHerd(AnimalNames.SHEEP, 5);
+    player.theHerd.addAnimalsToHerd(AnimalNames.PIG, 5);
+    player.theHerd.addAnimalsToHerd(AnimalNames.COW, 2);
+    player.theHerd.addAnimalsToHerd(AnimalNames.HORSE, 1);
+    const modal = new TradeModal(trade, player);
     Render.render('#sf-app', modal.createModal());
+    modal.setPlayer(player);
   }
 }
