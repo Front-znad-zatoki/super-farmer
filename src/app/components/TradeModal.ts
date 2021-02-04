4aa0b97467ef1527289ce1da4d6665ce4fdcb77a
Dominik <dominik-nowak@hotmail.com>
2021-02-04 06:10:54 +0100

    continuation of work on TradeModal

--- src/app/components/TradeModal.ts
+++ src/app/components/TradeModal.ts
@@ -1,70 +1,113 @@
+import { AnimalNames } from '~src/Enums/AnimalNamesEnum';
 import { Player } from '~src/Player';
 import { Herd } from '../logic/Herd';
-import { Trade } from '../Trade';
+import { Offer, Trade } from '../Trade';
 import { Render } from '../utils/Render';
 import { EmptyModal } from './EmptyModal';
 
 export class TradeModal extends EmptyModal {
-  tradeWrapper: HTMLElement;
+  tradeForm: HTMLElement;
   playerView: HTMLElement;
   bankView: HTMLElement;
-  constructor(private trade: Trade) {
+
+  constructor(private trade: Trade, private player: Player) {
     super();
     this.playerView = Render.elementFactory('div', {
-      className: 'trade-player__herd',
+      className: 'trade__player-wrapper',
     });
     this.bankView = Render.elementFactory('div', {
-      className: 'trade-player__herd',
+      className: 'trade__player-wrapper',
     });
-    this.tradeWrapper = Render.elementFactory(
-      'div',
+    this.tradeForm = Render.elementFactory(
+      'form',
       {
-        className: 'trade__wrapper',
+        action: '',
+        method: 'get',
+        className: 'trade',
       },
       this.playerView,
+      Render.elementFactory('input', {
+        type: 'submit',
+        value: 'Trade',
+        className: 'trade__submit',
+      }),
       this.bankView,
     );
+    this.modalContainer.appendChild(this.tradeForm);
   }
 
   createModal(): HTMLElement {
     Render.childrenInjector(
-      this.modalContainer,
-      this.createHerdView(new Player('Johny')),
+      this.bankView,
+      this.createHerdView(this.trade.thisBank, true),
     );
+    this.tradeForm.addEventListener('submit', this.handleSubmit);
     return this.modal;
   }
 
-  createHerdView(player: Player): HTMLElement {
-    const herd = Render.elementFactory(
+  setPlayer(player: Player): void {
+    Render.removeAllChildren(this.playerView);
+    this.playerView.appendChild(this.createHerdView(player));
+  }
+
+  refreshHerdViews(player: Player, bank: Player): void {
+    this.setPlayer(player);
+    Render.removeAllChildren(this.bankView);
+    this.bankView.appendChild(this.createHerdView(bank, true));
+  }
+
+  private createHerdView(
+    { theHerd: herd, theName: name }: Player,
+    isBank = false,
+  ): HTMLElement {
+    const herdElement = Render.elementFactory(
       'div',
       {
-        className: 'trade-player__herd',
+        className: 'trade__player-herd',
       },
-      ...this.generateAnimalRows(player.theHerd),
+      ...this.generateAnimalRows(herd, isBank),
     );
     const container = Render.elementFactory(
       'div',
       {
-        className: 'trade-player',
+        className: 'trade__player',
       },
       Render.elementFactory(
         'H2',
-        { className: 'trade-player__header' },
-        `${player.theName}`,
+        { className: 'trade__player-heading' },
+        `${name}`,
       ),
-      herd,
+      herdElement,
     );
     return container;
   }
 
-  private generateAnimalRows(herd: Herd): HTMLElement[] {
+  private generateAnimalRows(
+    herd: Herd,
+    isBank: boolean,
+  ): HTMLElement[] {
     const animalsRows: HTMLElement[] = herd.theAnimals.reduce(
       (animalsElements: HTMLElement[], [animal, count]) => {
         animalsElements.push(
           Render.elementFactory(
-            'p',
-            {},
-            `${animal.theName}: ${count}`,
+            'div',
+            { className: 'trade__row' },
+            Render.elementFactory(
+              'label',
+              {
+                for: `${isBank ? 'bank' : 'player'}_${
+                  animal.theName
+                }`,
+              },
+              `${animal.theName}: ${count}`,
+            ),
+            Render.elementFactory('input', {
+              type: 'number',
+              id: `${isBank ? 'bank' : 'player'}_${animal.theName}`,
+              name: `${isBank ? 'bank' : 'player'}_${animal.theName}`,
+              min: '0',
+              max: `${count}`,
+            }),
           ),
         );
         return animalsElements;
@@ -73,4 +116,38 @@ export class TradeModal extends EmptyModal {
     );
     return animalsRows;
   }
+
+  private formDataIntoTuples(formData: FormData): [Offer, Offer] {
+    const offer: Offer[] = [];
+    const target: Offer[] = [];
+    for (const [key, value] of formData.entries()) {
+      const numberOfAnimals = parseInt(value.toString());
+      const [player, animal] = key.split('_');
+      if (numberOfAnimals > 0) {
+        switch (player) {
+          case 'player': {
+            offer.push([animal as AnimalNames, numberOfAnimals]);
+            break;
+          }
+          case 'bank': {
+            target.push([animal as AnimalNames, numberOfAnimals]);
+            break;
+          }
+        }
+      }
+    }
+    if (offer.length !== 1 || target.length !== 1) {
+      console.log('Za dużo zwiemrzątek');
+    }
+    return [offer[0], target[0]];
+  }
+
+  private handleSubmit = (event: Event): void => {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    const [offer, target] = this.formDataIntoTuples(formData);
+    if (this.trade.processOffer(offer, this.player, target)) {
+      this.refreshHerdViews(this.player, this.trade.thisBank);
+    }
+  };
 }
