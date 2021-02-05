dab3f87d2ea68fcde5c144fabcbcc333524c726c
VieraBoschkova <vieraboschkova@gmail.com>
2021-02-05 09:30:16 +0100

    fix: change arguments type

--- src/app/logic/dynamicGameConfiguration.ts
+++ src/app/logic/dynamicGameConfiguration.ts
@@ -100,7 +100,7 @@ export const dynamicGameConfiguration: GameConfigInterface = {
       name: AnimalNames.FOX,
       path: '/static/images/avatars/fox.png',
       kills: [AnimalNames.RABBIT],
-      isChasedAwayBy: [AnimalNames.SMALL_DOG],
+      isChasedAwayBy: AnimalNames.SMALL_DOG,
       exclamation:
         'Ring-ding-ding-ding-dingeringeding! Wa-pa-pa-pa-pa-pa-pow!',
       dice: [{ diceNumber: 1, probability: 1 }],
@@ -109,7 +109,7 @@ export const dynamicGameConfiguration: GameConfigInterface = {
       name: AnimalNames.WOLF,
       path: '/static/images/avatars/wolf.png',
       kills: [AnimalNames.SHEEP, AnimalNames.PIG, AnimalNames.COW],
-      isChasedAwayBy: [AnimalNames.BIG_DOG],
+      isChasedAwayBy: AnimalNames.BIG_DOG,
       exclamation: 'Auuuuuu!Grrrrr!',
       dice: [{ diceNumber: 2, probability: 1 }],
     },
