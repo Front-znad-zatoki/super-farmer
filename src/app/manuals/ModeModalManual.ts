5a1fa7cd2d6f1300509133fa4133c5e4a63d0030
Dominik <dominik-nowak@hotmail.com>
2021-02-06 10:13:56 +0100

    update ModeModalManual

--- src/app/manuals/ModeModalManual.ts
+++ src/app/manuals/ModeModalManual.ts
@@ -3,7 +3,9 @@ import { Render } from '../utils/Render';
 
 export class ModeModalManual {
   static playDemo(): void {
-    const modeModal = new ModeModal();
+    const modeModal = new ModeModal((playersData) =>
+      console.log(playersData),
+    );
 
     Render.render('#sf-app', modeModal.createModeModal());
   }
