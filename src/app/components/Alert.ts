d4061fd6d10ee93d0ec34c7f6ce3a78839386b52
Enessetere <muszalski.sebastian@gmail.com>
2021-02-10 10:11:27 +0100

    feat: add Alert component with update functionality

---
+++ src/app/components/Alert.ts
@@ -0,0 +1,53 @@
+import { AlertType } from '../../Enums/AlertEnum';
+import { Render } from '../utils/Render';
+
+export class Alert {
+  private static alert: HTMLElement;
+
+  /**
+   * Creates and returns Alert component
+   * Usage: Alert.createElement()
+   */
+  static createElement(): HTMLElement {
+    this.alert = Render.elementFactory('div', {
+      id: 'alert-component',
+      className: 'alert',
+    });
+    return this.alert;
+  }
+
+  /**
+   * Updates Alert component with given message and alert level
+   * Usage: Alert.updateAlert(message, alertType)
+   * @param message accepts string with message to be displayed
+   * @param alertType accepts AlertType enum with correct level (INFO, WARN, CRITICAL)
+   */
+  static updateAlert(message: string, alertType: AlertType): void {
+    Render.removeAllChildren(this.alert);
+    switch (alertType) {
+      case AlertType.INFO:
+        this.alert.style.borderColor = 'blue';
+        break;
+      case AlertType.WARN:
+        this.alert.style.borderColor = 'yellow';
+        break;
+      case AlertType.CRITICAL:
+        this.alert.style.borderColor = 'red';
+        break;
+      default:
+        this.alert.style.borderColor = 'white';
+        break;
+    }
+    const icon = Render.elementFactory('img', {
+      className: 'alert__icon',
+      src: `./static/images/ui/${alertType}.png`,
+      alt: `${alertType}-alert`,
+    });
+    const text = Render.elementFactory(
+      'div',
+      { className: 'alert__text' },
+      `${message}`,
+    );
+    Render.childrenInjector(this.alert, icon, text);
+  }
+}
