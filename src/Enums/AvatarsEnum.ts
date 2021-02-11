debcb767ac74556fb1b9ad4b48850126f3284e40
Dominik <dominik-nowak@hotmail.com>
2021-02-11 02:09:42 +0100

    add color choice

--- src/Enums/AvatarsEnum.ts
+++ src/Enums/AvatarsEnum.ts
@@ -1,6 +1,6 @@
 export enum Avatars {
-  farmer1 = '.\\static\\images\\playerAvatars\\farmer1.svg',
-  farmer2 = '.\\static\\images\\playerAvatars\\farmer2.svg',
-  farmer3 = '.\\static\\images\\playerAvatars\\farmer3.svg',
-  farmer4 = '.\\static\\images\\playerAvatars\\farmer4.svg',
+  FARMER1 = '.\\static\\images\\playerAvatars\\farmer1.svg',
+  FARMER2 = '.\\static\\images\\playerAvatars\\farmer2.svg',
+  FARMER3 = '.\\static\\images\\playerAvatars\\farmer3.svg',
+  FARMER4 = '.\\static\\images\\playerAvatars\\farmer4.svg',
 }
