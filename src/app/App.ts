7c635a1d643d28a415874084623b6c41f15b21ee
VieraBoschkova <vieraboschkova@gmail.com>
2021-02-12 00:13:43 +0100

    fix: bring back app.ts to basic state

--- src/app/App.ts
+++ src/app/App.ts
@@ -3,7 +3,6 @@ import { ViewController } from './ViewController';
 export class App {
   init(): void {
     const view = new ViewController();
-    // logGameObject();
     view.displayMenuView();
   }
 }
