9e4798b023041aaa46d8a30207c7bd0c7f433f4b
Aleksandra Pawlak <synowa8@gmail.com>
2021-02-13 13:04:08 +0100

    Refresh App.ts

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,14 +1,8 @@
-import { BankBoard } from './components/BankBoard';
-import { Bank } from './logic/Bank';
-import { Render } from './utils/Render';
 import { ViewController } from './ViewController';
 
 export class App {
   init(): void {
-    const bank = new BankBoard();
-    const bankView = bank.renderBankBoard(new Bank());
-    Render.render('#sf-app', bankView);
-    // const view = new ViewController();
-    // view.displayMenuView();
+    const view = new ViewController();
+    view.displayMenuView();
   }
 }
