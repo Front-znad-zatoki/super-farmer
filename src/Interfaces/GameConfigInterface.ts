f1547aa9f680077f7b979139577ecae61b9b336c
VieraBoschkova <vieraboschkova@gmail.com>
2021-02-14 22:23:22 +0100

    fix: update playersDTO

--- src/Interfaces/GameConfigInterface.ts
+++ src/Interfaces/GameConfigInterface.ts
@@ -2,12 +2,13 @@ import { GameModes } from '../Enums/GameModeEnums';
 import { LivestockConfigInterface } from './LivestockConfigInterface';
 import { PredatorsConfigInterface } from './PredatorsConfigInterface';
 import { ProtectorsConfigInterface } from './ProtectorsConfigInterface';
+import { PlayerDTO } from './PlayerDTOInterface';
 
 export interface GameConfigInterface {
   mode: GameModes;
   roundTimeInSeconds: number;
   totalGameTimeInSeconds?: number;
-  playersConfig: { name: string; path: string; color: string }[];
+  playersConfig: PlayerDTO[];
   livestockConfig: LivestockConfigInterface[];
   protectorsConfig: ProtectorsConfigInterface[];
   predatorsConfig: PredatorsConfigInterface[];
