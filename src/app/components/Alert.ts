2108c0af868298ad68f2acfd8dfa6378feaef588
VieraBoschkova <vieraboschkova@gmail.com>
2021-02-14 23:26:46 +0100

    fix: unify all empty spaces between components

--- src/app/components/Alert.ts
+++ src/app/components/Alert.ts
@@ -31,13 +31,19 @@ export class Alert {
     Render.removeAllChildren(this.alert);
     switch (alertType) {
       case AlertType.INFO:
-        this.alert.style.backgroundColor = '#3E8Ed0D4';
+        this.alert.style.background =
+          'linear-gradient(to right, #3E8Ed0D4, #fff0)';
+        // '#3E8Ed0D4';
         break;
       case AlertType.WARN:
-        this.alert.style.backgroundColor = '#FFE08AD4';
+        this.alert.style.background =
+          'linear-gradient(to right, #FFE08AD4, #fff0)';
+        // '#FFE08AD4';
         break;
       case AlertType.CRITICAL:
-        this.alert.style.backgroundColor = '#F14668D4';
+        this.alert.style.background =
+          'linear-gradient(to right, #F14668D4, #fff0)';
+        // '#F14668D4';
         break;
       default:
         this.alert.style.backgroundColor = 'transparent';
