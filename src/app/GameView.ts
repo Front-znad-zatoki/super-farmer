a447b923018e4f638c35a003d46caa6d29fcbca3
paula0403 <paulinalipinskagdy@gmail.com>
2021-02-14 08:56:47 +0100

    GameView changed

--- src/app/GameView.ts
+++ src/app/GameView.ts
@@ -1,15 +1,16 @@
 import { AnimalNames } from '../Enums/AnimalNamesEnum';
 import { Player } from '../Player';
+import { Alert } from './components/Alert';
 import { BankBoard } from './components/BankBoard';
 import { PlayerPanel } from './components/PlayerPanel';
 import { PlayersBoard } from './components/PlayersBoard';
 import { Bank } from './logic/Bank';
 import { Render } from './utils/Render';
 import { ViewController } from './ViewController';
+import { AlertType } from '../Enums/AlertEnum';
 
 export class GameView {
-  private playerPanel: PlayerPanel;
-  // TODO: FIND OUT WHY THIS CREATES INSTANCE OF PLAYER PANEL IN CONTRUCTOR
+  playerPanel: PlayerPanel;
   constructor(private view: ViewController) {
     this.playerPanel = new PlayerPanel(this);
   }
@@ -19,33 +20,58 @@ export class GameView {
     currentPlayer: Player,
     bank: Bank,
   ): void {
-    const playersBoards = this.createPlayersBoards(players);
-    const playerPanel = this.createPlayerPanel(currentPlayer);
-    const endGameButton = this.createEndGameButton();
-    const bankPanel = this.createBankPanel(bank);
+    const topRow = this.createTopRow();
+    const gameBoardsAndPanel = this.createGameBoardsAndPanel(
+      players,
+      currentPlayer,
+      bank,
+    );
+
     Render.removeAllChildren('#sf-app');
     Render.render(
       '#sf-app',
       Render.elementFactory(
         'div',
-        { className: 'page-container' },
-        playersBoards,
-        playerPanel,
-        Render.elementFactory(
-          'div',
-          { className: 'bank__bar' },
-          endGameButton,
-          Render.elementFactory(
-            'div',
-            { id: 'bank-board' },
-            bankPanel,
-          ),
-        ),
+        { className: 'game' },
+        topRow,
+        gameBoardsAndPanel,
       ),
     );
-    this.setBackground(currentPlayer);
+    this.setColorAccents(currentPlayer);
   }
 
+  private createTopRow() {
+    const alertPanel = this.createAlertPanel();
+    const endGameButton = this.createEndGameButton();
+    const topRow = Render.elementFactory(
+      'div',
+      { className: 'game__top-row' },
+      alertPanel,
+      endGameButton,
+    );
+    return topRow;
+  }
+  private createGameBoardsAndPanel(
+    players: Player[],
+    currentPlayer: Player,
+    bank: Bank,
+  ) {
+    const playersBoards = this.createPlayersBoards(players);
+    const playerPanel = this.createPlayerPanel(currentPlayer);
+    const bankPanel = this.createBankPanel(bank);
+    const banksAndPanel = Render.elementFactory(
+      'div',
+      { className: 'game__side-panel' },
+      bankPanel,
+      playerPanel,
+    );
+    return Render.elementFactory(
+      'div',
+      { className: 'game__main' },
+      playersBoards,
+      banksAndPanel,
+    );
+  }
   private createPlayersBoards(players: Player[]): HTMLElement {
     return Render.elementFactory(
       'div',
@@ -54,7 +80,8 @@ export class GameView {
         Render.elementFactory(
           'div',
           {
-            id: `${player.theName}`,
+            // TODO: CHECK IF THIS DIV IS NECESSARY
+            // id: `${player.theName}`,
             className: 'player-boards__board',
           },
           new PlayersBoard().renderPlayersBoard(player),
@@ -64,8 +91,7 @@ export class GameView {
   }
 
   private createPlayerPanel(player: Player): HTMLElement {
-    this.playerPanel.setPlayer(player);
-    return this.playerPanel.createPlayerPanel();
+    return this.playerPanel.createPlayerPanel(player);
   }
 
   private createEndGameButton() {
@@ -84,10 +110,23 @@ export class GameView {
     return new BankBoard().renderBankBoard(bank);
   }
 
-  private setBackground(player: Player): void {
-    (document.querySelector(
-      '.page-container',
-    ) as HTMLElement).style.background = player.theColor;
+  private createAlertPanel(): HTMLElement {
+    let alertContainer = document.querySelector(
+      '.alert',
+    ) as HTMLElement;
+    if (!alertContainer) alertContainer = Alert.createElement();
+    // TODO: connect with other methods to display the right alert
+    Alert.updateAlert('Lorem ipsum dolor sei', AlertType.INFO);
+    return alertContainer;
+  }
+
+  private setColorAccents(player: Player): void {
+    document
+      .querySelectorAll('.player-panel__buttons .button')
+      .forEach((element) => {
+        (element as HTMLElement).style.borderColor = player.theColor;
+      });
+    // TODO: change to borders and font colors
   }
 
   handleRoll(): void {
@@ -100,22 +139,27 @@ export class GameView {
 
   displayRollResult(
     diceResults: AnimalNames[],
-    // playerGain: [AnimalNames, number][],
+    playerGain: [AnimalNames, number][],
+    player: Player,
   ): void {
-    this.playerPanel.displayRollResult(diceResults);
+    this.playerPanel.displayRollResult(
+      diceResults,
+      playerGain,
+      player,
+    );
     // this.renderPlayerPanel();
   }
 
   // private renderPlayerPanel(): void {
-  // Render.removeAllChildren('#player-board');
-  // Render.render(
-  // '#player-board',
-  // ...this.playerPanel.createPanelBoard(),
-  // );
+  //   Render.removeAllChildren('#player-board');
+  //   Render.render(
+  //     '#player-board',
+  //     ...this.playerPanel.createPanelBoard(),
+  //   );
   // }
 
   // updateRemainingTime(timeLeft: number): void {
-  // this.playerPanel.updateTime(timeLeft);
+  //   this.playerPanel.updateTime(timeLeft);
   // }
 
   stopTimer(): void {
@@ -127,7 +171,7 @@ export class GameView {
   }
 
   // turnAlert(): void {
-  // this.playerPanel.turnAlert();
+  //   this.playerPanel.turnAlert();
   // }
 
   pauseTurn(): void {
@@ -138,9 +182,9 @@ export class GameView {
     this.playerPanel.disableTrade();
   }
 
-  refreshHerd(bank: Bank): void {
-    // this.playerPanel.refreshHerd();
-    Render.removeAllChildren('#bank-board');
-    Render.render('#bank-board', this.createBankPanel(bank));
-  }
+  // refreshHerd(bank: Bank): void {
+  //   this.playerPanel.refreshHerd();
+  //   Render.removeAllChildren('#bank-board');
+  //   Render.render('#bank-board', this.createBankPanel(bank));
+  // }
 }
