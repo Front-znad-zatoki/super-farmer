ea57fcb65ac7e5a5e7493a3b17abc1f32f5da885
paula0403 <paulinalipinskagdy@gmail.com>
2021-02-15 09:45:38 +0100

    styles changed

--- src/app/components/PlayersBoard.ts
+++ src/app/components/PlayersBoard.ts
@@ -1,6 +1,8 @@
 import { Player } from '../../Player';
 import { Render } from '../utils/Render';
 import { flatten } from 'lodash';
+import { ConvertAnimalName } from '../utils/ConvertAnimalName';
+// import { GameView } from '../GameView';
 
 export class PlayersBoard {
   /*returns player's board with player's name, avatar and herd */
@@ -9,6 +11,7 @@ export class PlayersBoard {
       'div',
       {
         className: 'board-container',
+        style: `border:4 px solid${player.theColor}`,
       },
     );
     const playersDataPanel: HTMLElement = Render.elementFactory(
@@ -29,15 +32,29 @@ export class PlayersBoard {
     });
     const playerTimer: HTMLElement = Render.elementFactory(
       'div',
-      { className: 'player-data__time' },
+      { className: 'players__data__time' },
       `Time left`,
     );
 
-    const herdView: HTMLElement = Render.elementFactory('div', {
+    // const herdView: HTMLElement = Render.elementFactory('div', {
+    // className: 'players__herd',
+    // });
+    const herdView = Render.elementFactory('div', {
       className: 'players__herd',
     });
-
-    const playerHerd: [
+    const playerHerd = player.theHerd.theAnimals.map(
+      ([animal, count]) =>
+        Render.elementFactory(
+          'div',
+          { className: 'players__herd__item' },
+          ConvertAnimalName.toHTMLElement(
+            animal.theName,
+            'players__herd__item__img',
+          ),
+          `${count}`,
+        ),
+    );
+    /*[
       string,
       number,
     ][] = player.theHerd.theAnimals.map(([animal, count]) => [
@@ -59,25 +76,24 @@ export class PlayersBoard {
 
         return [animalImg, animalCount];
       },
-    );
-    Render.childrenInjector(
-      herdView,
-      ...flatten(herdImagesAndCounts),
-    );
-
+    );*/
+    // Render.childrenInjector(
+    // herdView,
+    // ...flatten(herdImagesAndCounts),
+    // )
     Render.childrenInjector(
       playersDataPanel,
       playerName,
       playerAvatar,
       playerTimer,
     );
+    Render.childrenInjector(herdView, ...playerHerd);
     Render.childrenInjector(
       playersBoardContainer,
-      playersDataPanel,
       herdView,
+      playersDataPanel,
     );
-    /*
-    this.view.stopTimer();
+    /*stopTimer();
     setTimeout(() => this.hideTimer(), 10);
   }
 
@@ -85,34 +101,17 @@ export class PlayersBoard {
     (document.querySelector(
       '#time-left',
     ) as HTMLElement).style.display = 'none';
-  }
-
-  /**
-   * Updates timer on player panel
-   * @param timeLeft accepts number value for time left
-   */
-    /*updateTime(timeLeft: number): void {
-    const timer = document.querySelector('#time-left') as HTMLElement;
-    timer.innerText = `Time left: ${timeLeft} sec.`;
-  }
-
-  turnAlert(): void {
-    Render.render(
-      '#sf-app',
-      Render.elementFactory(
-        'div',
-        { className: 'exclamation' },
-        `${player.}'s turn has passed!`,
-      ),
-    );
-  }
-
-  disableTrade(): void {
-    (document.querySelector('#exchange') as HTMLElement).setAttribute(
-      'disabled',
-      'true',
-    );
   }*/
+
+    /**
+     * Updates timer on players board
+     * @param timeLeft accepts number value for time left
+     */
+    // updateTime(timeLeft: number): void {
+    // const timer = document.querySelector('#time-left') as HTMLElement;
+    // timer.innerText = `Time left: ${timeLeft} sec.`;
+    // }
+
     return playersBoardContainer;
   }
 }
