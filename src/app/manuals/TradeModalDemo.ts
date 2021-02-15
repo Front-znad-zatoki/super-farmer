d5bb45406cd91cd2c413997ec609403cf33fb7e6
Enessetere <muszalski.sebastian@gmail.com>
2021-02-15 22:13:34 +0100

    fix: add dependency in manuals

--- src/app/manuals/TradeModalDemo.ts
+++ src/app/manuals/TradeModalDemo.ts
@@ -3,6 +3,7 @@ import { Player } from '~src/Player';
 import { TradeModal } from '../components/TradeModal';
 import { Bank } from '../logic/Bank';
 import { defaultGameConfiguration } from '../logic/defaultGameConfiguration';
+import { defaultPlayerHerdConfig } from '../logic/defaultHerdConfig';
 import { Trade } from '../Trade';
 import { Render } from '../utils/Render';
 import { ViewController } from '../ViewController';
@@ -27,6 +28,7 @@ export class TradeModalDemo {
     const modal = new TradeModal(
       trade,
       player,
+      defaultPlayerHerdConfig,
       () => view.runTimer(),
       () => view.processAfterTrade(),
     );
